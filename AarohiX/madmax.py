0c32321d5cd0cee6827157ccb60f5904a012a9f7
THEMADMAXOP <156558024+THEMADMAXOP@users.noreply.github.com>
2024-02-03 16:01:17 +0530

    Add files via upload

---
+++ AarohiX/madmax.py
@@ -0,0 +1,19 @@
+STICKER = [
+    "CAACAgUAAxkDAAIKr2WtxzrI0va3-sACUUiSKCFkg_GkAAK1AgACioWJVgNtVr8dAAGApx4E",
+    "CAACAgUAAxkBAAIK9GWtx1UR7qjxJWuiuuaBBalcY9OmAAKBAgACEX-IVufdvEo584z1HgQ",
+    "CAACAgUAAxkBAAIK82Wtx00aD6p6M2AbIN4jRCZm0h5XAAKqAgACc9CAVmuSv5M1PCzkHgQ",
+]
+
+
+EMOJIOS = [
+    "💣",
+    "💥",
+    "🪄",
+    "🧨",
+    "⚡",
+    "🤡",
+    "👻",
+    "🎃",
+    "🤤",
+    "💞",
+]
